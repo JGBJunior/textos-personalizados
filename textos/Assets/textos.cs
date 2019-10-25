4904ad88170332e01ecb6f08d838a1b4678f74ba
JGBJunior <jgbjunior99@gmail.com>
2019-10-25 17:01:19 +0200

    <textos>

---
+++ textos/Assets/textos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Este es un ejemplo de texto enriquecido <b>con negrita</b>.");
+		Debug.Log ("Este es un ejemplo de texto enriquecido <b><i>con negrita y cursiva</i></b>.");
+		Debug.Log ("Este es un ejemplo de texto <i><color=red>con cursiva y en rojo</color></i>.");
+		Debug.Log ("Este es un ejemplo de texto <b><color=red>con negrita y en rojo</color></b>.");
+		Debug.Log ("Este es un ejemplo de texto <size=20><color=green>en verde y a 20 pixeles</color></size>.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
